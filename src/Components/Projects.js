09888a52e7f609fc4f8c0414dc79b29a2264dab4
Adeboye Bello <Boyedavid@icloud.com>
2025-07-12 22:34:47 +0100

    first commit

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -35,7 +35,7 @@ const Projects = () => {
                     <span key={index}
                     className="mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800">
                         {tech}
-                    </span>
+                    </span> 
                 ))}
             </motion.div>
         </div>
